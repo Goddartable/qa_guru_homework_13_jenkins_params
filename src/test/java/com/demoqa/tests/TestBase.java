0ec56f19b741d981c861acc40c6bb0cca073a705
Goddartable <goddart777@gmail.com>
2023-08-31 21:37:59 +0300

    v3

--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -4,10 +4,14 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import com.demoqa.attachments.ReportAttachments;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.AfterEach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class TestBase {
@@ -18,10 +22,13 @@ public class TestBase {
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
-       /* DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-        Configuration.browserCapabilities = capabilities;*/
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        Map<String, Object> value = new HashMap<>();
+        value.put("enableVNC", true);
+        value.put("enableVideo", true);
+        capabilities.setCapability("selenoid:options", value);
+
+        Configuration.browserCapabilities = capabilities;
 
     }
 
@@ -29,10 +36,15 @@ public class TestBase {
     @AfterEach
     void afterEachTest() {
         SelenideLogger.addListener("Allure", new AllureSelenide());
-        Selenide.closeWebDriver();
+
+
         ReportAttachments.addVideo();
         ReportAttachments.attachScreenshot();
         ReportAttachments.pageSource();
         ReportAttachments.browserConsoleLogs();
+
+        Selenide.closeWebDriver();
+
+
+        }
     }
-}
