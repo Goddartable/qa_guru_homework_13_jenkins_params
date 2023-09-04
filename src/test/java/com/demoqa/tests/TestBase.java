2eaf749da2f2e21029b1a2dd734795bcec5f52cd
Goddartable <goddart777@gmail.com>
2023-09-04 19:18:08 +0300

    get property

--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -20,7 +20,7 @@ public class TestBase {
         SelenideLogger.addListener("Allure", new AllureSelenide());
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        Configuration.browserVersion = System.getProperty("browserVersion", "85.0");
+        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
         Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
